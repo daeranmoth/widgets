75330f68c4debaff89f3f574d236c114bebec4bb
gary camboulives <gary.camboulives@gmail.com>
2022-06-07 17:25:25 +0200

    Searchbar component with 2 useEffect and 3 useState hooks

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,38 +3,36 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('programming');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
-    const search = async() => {
+    const search = async () => {
       const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
 
       setResults(data.query.search);
     };
+    search();
+  }, [debouncedTerm]);
 
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-      if (term) {
-        search();
-      }
-    },1000);
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }
- }, [term]);
 //empty array = arrow function code is run at initial render,
 //nothing = arrow function code is run at initial render + after every rerender
 //an array with data = arrow function code is run at initial render + after every rerender IF data has changed since last render
