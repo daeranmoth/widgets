1c00f5d55ca8c8655c4e87ef0966bb0edd0f4ad1
gary camboulives <gary.camboulives@gmail.com>
2022-06-08 22:00:56 +0200

    debounce text with useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-// import Accordion from './components/Accordion';
+import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from "./components/Translate";
 
 const items = [
   {
@@ -34,18 +35,10 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
+
   return (
   <div>
-    <button onClick={() => setShowDropdown(!showDropdown)}>Toggle dropdown</button>
-    {showDropdown ? (
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
-    ) : null}
+    <Translate />
   </div>
   );
 };
